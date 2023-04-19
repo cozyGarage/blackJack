ecb4cceb42effe08c58be8e214a1f18a979594e1
cozyGarage <personal.trungnguyen@gmail.com>
2023-04-19 17:08:23 +0300

    Improve the code as in readme

--- blackjack.js
+++ blackjack.js
@@ -1,47 +1,49 @@
-var dealerSum = 0;
-var playerSum = 0;
-var dealerAceCount = 0;
-var playerAceCount = 0;
-var hidden;
-var deck;
-var canHit = true; // allows the player to draw while yourSum<21
+let dealerSum = 0;
+let playerSum = 0;
+let dealerAceCount = 0;
+let playerAceCount = 0;
+let hidden;
+let deck;
+let canHit = true; // allows the player to draw while yourSum<21
 
-window.onload = function(){
+const dealerCards = document.getElementById("dealer-cards");
+const playerCards = document.getElementById("player-cards");
+const dealerSumDisplay = document.getElementById("dealer-sum");
+const playerSumDisplay = document.getElementById("player-sum");
+const resultsDisplay = document.getElementById("results");
+const hitButton = document.getElementById("hit");
+const stayButton = document.getElementById("stay");
+
+window.addEventListener("load", () => {
     buildDeck();
     shuffleDeck();
     startGame();
-}
+  });
 
 function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
-    let types = ["C", "D", "H", "S"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
+    const types = ["C", "D", "H", "S"];
     deck = [];
-    for (let i = 0; i<types.length; i++) {
-        for (let j = 0; j<values.length; j++){
-            deck.push(`${values[j]}-${types[i]}`); // A-C ---K-C, A---D  --> K-D
-        }
+    for (let i = 0; i < types.length; i++) {
+      for (let j = 0; j < values.length; j++){
+        deck.push(`${values[j]}-${types[i]}`);
+      }
     }
-    //console.log (deck);
 }
 
 function shuffleDeck(){
-    for(let i=0; i < deck.length; i++){
-        let j = Math.floor(Math.random()* deck.length); //(0-1) * 52 => 0-51.99
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
+    for(let i = 0; i < deck.length; i++){
+        const j = Math.floor(Math.random() * deck.length);
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+      }
     }
-    console.log(deck);
-}
 
 function startGame(){
         hidden = deck.pop();
         dealerSum += getValue(hidden);
         dealerAceCount += checkAce(hidden);
-        //console.log(hidden);
-        //console.log(dealerSum);
+        
         while (dealerSum<17){
-            // <img src="./cards/4-C.png">
             let cardImg = document.createElement("img");
             let card = deck.pop();
             cardImg.src = "./cards/" + card + ".png";
